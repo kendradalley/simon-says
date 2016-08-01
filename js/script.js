134cc549ffd44fcfb39bf82ccd9228a8539ebef3
kendradalley <kendracdalley@gmail.com>
2016-08-01 15:51:42 -0700

    created basic simon says board

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+// define variables
+  var userPattern = [];
+  var computerChoice = [];
+  var round = ''; //round starts at 0
+  var result = '';
+
+
+
+$()
+
+
+// get random choice from computer
+function getComputerChoice() {
+  var choices = ['red', 'green', 'blue', 'yellow'];
+  var randomIndex = [Math.floor(Math.random() * choices.length)];
+
+  return choices[randomIndex];
+}
+
+
+}
